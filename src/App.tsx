cf38269762f2dda729116c5e83402b6821a7b802
xin.zhao <xin.zhao@datacvg.com>
2022-12-14 10:33:49 +0800

    react+ts创建项目

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import {Button} from 'antd'
+// import 'antd/dist/antd.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Button type='primary'>测试</Button>
+      顶级组件
+    </div>
+  )
+}
+
+export default App
